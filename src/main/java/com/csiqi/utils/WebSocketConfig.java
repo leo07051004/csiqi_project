e747739563ca83da6281e89acf9af84cc41448d6
leo07051004 <975121990@qq.com>
2019-10-30 17:34:24 +0800

    webSocket

---
+++ src/main/java/com/csiqi/utils/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.csiqi.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by kd-user47823 on 2019/10/30.
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
